c3f315accde3a32d7768e5032267669d0f3ba753
yusrinulkarimah <yusrinulkarimah@gmail.com>
2026-08-27 19:48:35 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('fetch',()=>{});
+const C='dapur-ibu-v2';const A=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
