e1462f19ed31e0cdbe17b989606fccb798f08d12
yusrinulkarimah <yusrinulkarimah@gmail.com>
2026-09-09 17:33:06 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,11 @@
-const CACHE='dapur-ibu-v34';const F=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='dapur-ibu-realtime-ready-1';
+const F=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(F))).then(()=>self.skipWaiting()));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))).then(()=>self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin){return;}
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
+    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
+  }).catch(()=>caches.match(e.request)));
+});
