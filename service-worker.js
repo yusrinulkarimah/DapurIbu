bf7a713952bd5e8f85398dc2a3285035a5db0429
yusrinulkarimah <yusrinulkarimah@gmail.com>
2026-09-09 17:38:15 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
-const CACHE='dapur-ibu-realtime-ready-1';
+const CACHE='dapur-ibu-realtime-final-1';
 const F=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(F))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim()));
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
-  if(u.origin!==location.origin){return;}
-  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
-    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
-  }).catch(()=>caches.match(e.request)));
+  if(u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
 });
